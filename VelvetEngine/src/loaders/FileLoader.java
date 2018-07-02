d007e4bfcd02438e9daf2ee9fa7ff6201ca848eb
NebulousDev <nebulousdev@gmail.com>
2018-07-02 13:21:18 -0400

    FileLoader now loads Bytes
    7/2/18 1:21PM
    - FileLoader now loads byes

--- VelvetEngine/src/loaders/FileLoader.java
+++ VelvetEngine/src/loaders/FileLoader.java
@@ -5,19 +5,20 @@ import java.io.InputStream;
 
 public class FileLoader
 {
-	public static String readFileAsString(String filepath)
+	public static byte[] readFileAsBytes(String filepath)
 	{
 		byte[] buffer = new byte[1024];
 		InputStream fileStream = FileLoader.class.getResourceAsStream(filepath);
 		ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
 		
+		System.out.println("Reading : " + filepath);
+		
 		try
 		{
 			int bytes = 0;
 			while((bytes = fileStream.read(buffer)) > 0)
-			{
 				byteBuffer.write(buffer, 0, bytes);
-			}
+			fileStream.close();
 		}
 		
 		catch(Exception e)
@@ -26,7 +27,12 @@ public class FileLoader
 			e.printStackTrace();
 			return null;
 		}
-
-		return new String(byteBuffer.toByteArray());
+		
+		return byteBuffer.toByteArray();
+	}
+	
+	public static String readFileAsString(String filepath)
+	{
+		return new String(readFileAsBytes(filepath));
 	}
 }
